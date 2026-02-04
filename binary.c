a14fe3fe83902da0eacce6f1cf27672c085b8197
melshekh <mmahmoudelshekhali@gmail.com>
2026-02-04 22:29:29 +0100

    Implemented  test_bit and clear_bit functions

--- binary.c
+++ binary.c
@@ -1,9 +1,16 @@
 #include "binary.h"
 
-void set_bit(int x, int y, int** board){
+void set_bit(int x, int y, int* board){
+    int flag = 1;
+    flag << x - 1;
 
+    board[y-1] = board[y-1] | flag;
 }
 
-void clear_bit(int x, int y, int** board){
+void clear_bit(int x, int y, int* board){
+    int flag = 1;
+    flag << x - 1;
+    flag = ~flag;
 
+    board[y-1] = board[y-1] & flag;
 }
