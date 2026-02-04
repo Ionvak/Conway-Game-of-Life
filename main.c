d2ef2d7f12a5daff0e12d1ac0c5bace9ec2791ac
melshekh <mmahmoudelshekhali@gmail.com>
2026-02-04 22:31:15 +0100

    Fixed issue with array dimension in main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include "game.h"
 #include "binary.h"
-#define SIZE 256
+#define SIZE 16
 #define INPUT_FILE "gof.txt"
 int main(int argc, char** argv) {
-    int board[SIZE][SIZE];
+    int board[SIZE];
     initialize_board(board, INPUT_FILE);
 
     while(1){
@@ -12,3 +12,4 @@ int main(int argc, char** argv) {
     }    
     return 0;
 }
+
