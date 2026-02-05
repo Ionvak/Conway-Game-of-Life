0ed310a3939c90ea9547ece61b97489bad24c351
melshekh <mmahmoudelshekhali@gmail.com>
2026-02-05 22:11:29 +0100

    Fixed lack of assignment in left shift operator

--- binary.c
+++ binary.c
@@ -2,14 +2,14 @@
 
 void set_bit(int x, int y, int* board){
     int flag = 1;
-    flag << x - 1;
+    flag <<= x - 1;
 
     board[y-1] = board[y-1] | flag;
 }
 
 void clear_bit(int x, int y, int* board){
     int flag = 1;
-    flag << x - 1;
+    flag <<= x - 1;
     flag = ~flag;
 
     board[y-1] = board[y-1] & flag;
@@ -17,9 +17,9 @@ void clear_bit(int x, int y, int* board){
 
 int get_bit(int x, int y, int* board){
     int flag = 1;
-    flag << x - 1;
+    flag <<= x - 1;
 
-    if(flag & board[y-1])
+    if(board[y-1] & flag)
         return 1;
     else 
         return 0;
