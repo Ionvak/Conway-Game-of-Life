7d96a5f469bc0e18d72af8331bf5c742c9625bff
melshekh <mmahmoudelshekhali@gmail.com>
2026-02-05 22:46:14 +0100

    Implemented game.c functions with accompanying tests

--- game.c
+++ game.c
@@ -1,11 +1,54 @@
 #include "game.h"
+#include "binary.h"
+#include <stdio.h>
+#include <string.h>
+#define INT_SIZE 32
 
-void update(int* board){
+void update(int* board, int size){
+    int live_neighbors;
+    int bit_val;
+    int board_cpy[size];
+    memcpy(board_cpy, board, size * sizeof(int));
+    
+    for(int line = 1 ; line <= size; line++){
+        for(int bit = 1; bit <= INT_SIZE ; bit++){
+            bit_val = get_bit(bit, line, board_cpy);
+            live_neighbors = 0;
 
+            //top row. left, middle, right bits.
+            if(bit > 1        && line > 1    && get_bit(bit-1,line-1,board_cpy)) live_neighbors++;
+            if(                  line > 1    && get_bit(bit  ,line-1,board_cpy)) live_neighbors++;
+            if(bit < INT_SIZE && line > 1    && get_bit(bit+1,line-1,board_cpy)) live_neighbors++;
+
+            //middle row. left, right bits.
+            if(bit > 1        &&                get_bit(bit-1,line  ,board_cpy)) live_neighbors++;
+            if(bit < INT_SIZE &&                get_bit(bit+1,line  ,board_cpy)) live_neighbors++;
+
+            //bottom row. left, middle, right bits.
+            if(bit > 1        && line < size && get_bit(bit-1,line+1,board_cpy)) live_neighbors++;
+            if(                  line < size && get_bit(bit  ,line+1,board_cpy)) live_neighbors++;
+            if(bit < INT_SIZE && line < size && get_bit(bit+1,line+1,board_cpy)) live_neighbors++;
+
+            if(!bit_val && live_neighbors == 3)
+                set_bit(bit, line, board);  
+            else if(bit_val && (live_neighbors < 2 || live_neighbors > 3))
+                clear_bit(bit, line, board);
+        }     
+    }
 }
 
-void display(int* board){
-
+void display(int* board, int size){
+    printf("\e[1;1H\e[2J");
+    for(int line = 1 ; line <= size; line++){
+        for(int bit = INT_SIZE; bit >= 1 ; bit--){
+            int bit_val = get_bit(bit, line, board);
+            if(bit_val)
+                printf("# ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
 }
 
 void initialize_board(int* board, char* file_name){
--- tests.c
+++ tests.c
@@ -2,6 +2,7 @@
 #include "game.h"
 #include "binary.h"
 #define SIZE 16
+#define INT_SIZE 32
 #define INPUT_FILE "gof.txt"
 
 int set_bit_test(){
@@ -27,20 +28,34 @@ int clear_bit_test(){
 }
 
 void get_bit_test(){
-    int tested[1] = {1};
+    int tested[1] = {67};
 
-    printf("Bit Read Test\nActual Value: %d\n", tested[0]);
-    printf("get_bit result: %d\n", get_bit(1,1,tested));
+    printf("Bit Read Test\nActual Value:   00000000000000000000000001000011\n");
+    printf("get_bit result: ");
+    for(int i = INT_SIZE; i >= 1 ; i--)
+        printf("%d", get_bit(i,1,tested));
+    printf("\n");
+}
 
-    clear_bit(1,1,tested);
+void test_display(){
+    int board[SIZE] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    display(board, SIZE);
+}
 
-    printf("Actual Value: %d\n", tested[0]);
-    printf("get_bit result: %d\n", get_bit(1,1,tested));
+void test_update(){
+    int board[SIZE] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    printf("original board:\n");
+    display(board, SIZE);
+    printf("updated board:\n");
+    update(board, SIZE);
+    display(board, SIZE);
 }
 
 int main(int argc, char** argv) {
     printf("Set Bit Test Result: %d\n",set_bit_test());
     printf("Clear Bit Test Result: %d\n",clear_bit_test());
     get_bit_test();
+    test_display();
+    test_update();
     return 0;
 }
