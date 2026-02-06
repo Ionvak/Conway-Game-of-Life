8de8dd80255d543dfa5cfc83361f19b866485c4a
melshekh <mmahmoudelshekhali@gmail.com>
2026-02-06 21:44:50 +0100

    Added constant for refresh period

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 #include <unistd.h>
 #define SIZE 32
 #define INPUT_FILE "gof.txt"
+#define REFRESH_PERIOD_MS 300
 int main(int argc, char** argv) {
     int board[SIZE];
     initialize_board(INPUT_FILE, board);
 
     while(1){
         update(board, SIZE);
-        usleep(500000);
+        usleep(REFRESH_PERIOD_MS * 1000);
         display(board, SIZE);
     }    
     return 0;
