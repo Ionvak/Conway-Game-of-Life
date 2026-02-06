d999930fc6878cc850ac04cf0912517aaea610e1
melshekh <mmahmoudelshekhali@gmail.com>
2026-02-06 21:50:20 +0100

    changed REFRESH_PERIOD_MS to UPDATE_PERIOD_MS

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 #include <unistd.h>
 #define SIZE 32
 #define INPUT_FILE "gof.txt"
-#define REFRESH_PERIOD_MS 300
+#define UPDATE_PERIOD_MS 300
 int main(int argc, char** argv) {
     int board[SIZE];
     initialize_board(INPUT_FILE, board);
 
     while(1){
         update(board, SIZE);
-        usleep(REFRESH_PERIOD_MS * 1000);
+        usleep(UPDATE_PERIOD_MS * 1000);
         display(board, SIZE);
     }    
     return 0;
